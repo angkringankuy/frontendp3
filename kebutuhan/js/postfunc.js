b96f6c40187d5fe6a47ededbbc20832f0277d0c1
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-21 00:59:29 +0700

    chpppp

--- kebutuhan/js/postfunc.js
+++ kebutuhan/js/postfunc.js
@@ -5,7 +5,7 @@ import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js
 export default function PostSignUp(){
     let target_url = "https://asia-southeast2-rock-prism-401900.cloudfunctions.net/gcpproyek3";
     let tokenkey = "token";
-    let tokenvalue = "OSBNpWx2bFLLCLMryE1ux19CEUDw"; 
+    let tokenvalue = "40e27523381e236813ece16a5e420192592d2ed80cd74eafe115afbaebe874d2"; 
     let datainjson = {
         "username": getValue("username"),
         "password": getValue("password")
